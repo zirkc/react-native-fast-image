c3d60d4642e27968cd61389f6ef6e55424ed099a
Miro Metsänheimo <miro@metsanheimo.fi>
2023-08-25 12:03:14 +0300

    Increase disk size in android

--- android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
@@ -1,9 +1,22 @@
 package com.dylanvann.fastimage;
 
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 import com.bumptech.glide.module.AppGlideModule;
 
 // We need an AppGlideModule to be present for progress events to work.
 @GlideModule
 public final class FastImageGlideModule extends AppGlideModule {
+
+	@Override
+	public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+		int diskCacheSizeBytes = 1024 * 1024 * 1024; // 1 GB
+		builder.setDiskCache(new InternalCacheDiskCacheFactory(context, diskCacheSizeBytes));
+	}
+
 }
